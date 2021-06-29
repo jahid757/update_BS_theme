ce4bb7ed3393c4e028a1595319db25385c545f45
Jahid Hasan <slniloy500@gmail.com>
2021-06-29 21:29:30 +0600

    update property detail page

--- custom.js
+++ custom.js
@@ -13,3 +13,41 @@ function myFunction(id) {
       moreText.style.display = "inline";
     }
   }
+
+
+
+// scroll spy 
+
+window.onscroll = function() {scrollSpy()};
+
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
+
+function scrollSpy() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+$(function() {
+  const topOffset = 5; //variable for menu height
+
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar-nav-menu a').click(function() {
+  if (location.pathname.replace(/^\//,'') === 
+    this.pathname.replace(/^\//,'') && 
+    location.hostname === this.hostname) {
+    const target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+    $('html,body').animate({
+      scrollTop: target.offset().top-topOffset
+    }, 1500);
+    return false;
+    } //target.length
+  } //click function
+  }); //smooth scrolling
+
+});		
